da15da0cd01f83b35b6c10af2e60bb03bec9174a
Hau Nguyen <thehaung217@gmail.com>
2022-08-06 17:33:17 +0700

    chore: optimize resource

--- src/main/java/vn/thehaung/service/FruitService.java
+++ src/main/java/vn/thehaung/service/FruitService.java
@@ -1,23 +1,14 @@
 package vn.thehaung.service;
 
-import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
-import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
-import com.datastax.oss.driver.api.core.cql.SimpleStatement;
-import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
-import com.datastax.oss.driver.api.querybuilder.select.Select;
-import com.datastax.oss.driver.api.querybuilder.select.SelectFrom;
 import vn.thehaung.dao.FruitDao;
 import vn.thehaung.model.Fruit;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.List;
-
-import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.bindMarker;
-import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
 
 /**
  * @author : Hau Nguyen
